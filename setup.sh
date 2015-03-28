ec36697736ba2b55ede99daa3a153263e2164e5c
Diogo Carneiro <diogocarneiro@icloud.com>
2015-03-28 20:42:59 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,8 +4,5 @@ set -e
 
 cd "`dirname "$0"`"
 xcode-select --install
-if xcode-select = true then
-echo 'As dependêcias já se encontras instaladas!';
-else
 sudo gem install cheatset
 cheatset generate unrealengine.rb
